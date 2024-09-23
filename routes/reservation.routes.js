a790414c3e18d9c0e574b7f3dae5759d73ac033d
fati-byb <fatim.zehra.bouyoub@gmail.com>
2024-09-23 10:31:39 +0100

    no

---
+++ routes/reservation.routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const reservationController = require("../controllers/reservation/reservation.controller");
+
+router.post('/add-reservation', reservationController.createReservation);
+router.get('/get-reservation', reservationController.getReservation);
+router.put('/updateReservation/:id', reservationController.updateReservation);
+router.delete('/delete/:id', reservationController.deleteReservation);
+
+
+module.exports = router;
