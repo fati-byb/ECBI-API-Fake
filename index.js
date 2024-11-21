50972417f96af666d3e6e4b4f61a48cbb31b2ff4
H-Taliby <habibataliby@gmail.com>
2024-11-21 17:03:26 +0100

    add vercel.json file

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
     console.log(`Server is ready for connections on port ${PORT} `);
 });
+
+require('dotenv').config();
+const app = require('./app');
+
+// Export the app as a handler for Vercel
+module.exports = (req, res) => {
+  app(req, res); // Pass the request and response to your express app
+};
