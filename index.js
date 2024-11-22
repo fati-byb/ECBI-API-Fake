04e3022718c9a7ce7f8b09f7f3eafce86bc652dc
H-Taliby <habibataliby@gmail.com>
2024-11-22 11:23:14 +0100

    updated index js file

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-require('dotenv').config();
 
+require('dotenv').config();
 const app = require('./app');
 
-const server = require('http').createServer(app);
+// Export the app directly to be used as a serverless function on Vercel
+module.exports = app;
 
-const PORT = process.env.PORT || 4000;
+
+// require('dotenv').config();
+
+// const app = require('./app');
+
+// const server = require('http').createServer(app);
+
+// const PORT = process.env.PORT || 4000;
  
 
-server.listen(PORT, () => {
-    console.log(`Server is ready for connections on port ${PORT} `);
-});
+// server.listen(PORT, () => {
+//     console.log(`Server is ready for connections on port ${PORT} `);
+// });
 
-require('dotenv').config();
-const app = require('./app');
+// // require('dotenv').config();
+// // const app = require('./app');
 
-// Export the app as a handler for Vercel
-module.exports = (req, res) => {
-  app(req, res); // Pass the request and response to your express app
-};
+// // Export the app as a handler for Vercel
+// module.exports = (req, res) => {
+//   app(req, res); // Pass the request and response to your express app
+// };
