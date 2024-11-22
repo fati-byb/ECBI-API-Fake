9756b3b1ac3860fa34a05cf881e5ccc981eb71ac
H-Taliby <habibataliby@gmail.com>
2024-11-22 12:29:36 +0100

    updated index js file

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-require('dotenv').config();
-const app = require('./app');
+const app = require('express')();
+const { v4 } = require('uuid');
 
-// Export the app directly to be used as a serverless function on Vercel
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
 module.exports = app;
