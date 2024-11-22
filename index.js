fa5f6597440794560c222ee3289f45d697e69816
H-Taliby <habibataliby@gmail.com>
2024-11-22 12:16:16 +0100

    updated index js file

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 require('dotenv').config();
-
 const app = require('./app');
 
-const server = require('http').createServer(app);
-
-const PORT = process.env.PORT || 4000;
- 
-
-server.listen(PORT, () => {
-    console.log(`Server is ready for connections on port ${PORT} `);
-});
-
- 
-// Export the app as a handler for Vercel
-module.exports = (req, res) => {
-  app(req, res); // Pass the request and response to your express app
-};
+// Export the app directly to be used as a serverless function on Vercel
+module.exports = app;
