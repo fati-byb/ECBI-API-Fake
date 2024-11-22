34bec1169179f04a7f107cb081f841242c3d553f
H-Taliby <habibataliby@gmail.com>
2024-11-22 17:17:19 +0100

    updated index js file

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ const app = require('./app');
 
 // Export the app as a handler for Vercel
 module.exports = (req, res) => {
-  res.status(200).json({ message: "API is working!" });
+  // res.status(200).json({ message: "API is working!" });
   app(req, res); // Pass the request and response to your express app
 };
