127a888d5e04a0715ad6ddfe9b7b9248792a1177
H-Taliby <habibataliby@gmail.com>
2024-11-23 13:16:40 +0100

    api working message

--- index.js
+++ index.js
@@ -15,9 +15,13 @@ app.get('/api/category/get-categories', (req, res) => {
   res.json({ categories: categories });
 });
 
+app.get('/api/products/get-products', (req, res) => {
+  res.json();
+});
+
 
 // Export the app as a handler for Vercel
 module.exports = (req, res) => {
-  // res.status(200).json({ message: "API is working!" });
+  res.status(200).json({ message: "API is working!" });
   app(req, res); // Pass the request and response to your express app
 };
