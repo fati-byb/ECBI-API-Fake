a10f27d004b5a48237b9a3a29de5f823496b486e
H-Taliby <habibataliby@gmail.com>
2024-12-05 19:31:21 +0100

    google sheet, vercel, adalo,socket testing

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
  const path = require('path');
  const { io } = require('socket.io-client');
  const { fetchAndSendData } = require('./syncData');
- const socket = io('https://e802-105-158-84-51.ngrok-free.app');
+ const socket = io('https://e074-105-158-84-51.ngrok-free.app');
  // in order to create a google scheet
 
 
@@ -35,7 +35,7 @@ app.use('/media/images', express.static(path.join('./media/images')));
  
  //-------------- DB Config --------------//
 mongoose.connect(process.env.MONGODB_URI, {
-    serverSelectionTimeoutMS: 50000,
+    // serverSelectionTimeoutMS: 50000,
     // useNewUrlParser: true,
     //  useUnifiedTopology: true
 
